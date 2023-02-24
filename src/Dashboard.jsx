3e9e0e9867c383cb5443daa7873d9bb9cd0b602b
Danjune <daniel.ayala621@gmail.com>
2023-02-24 13:52:48 -0600

    actualizacion 1

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,111 +1,119 @@
 import DataTable, { createTheme } from 'react-data-table-component';
 import './Dashboard.css';
+import { useState } from 'react';
 
-
+//Styles
 const customStyles = {
-    rows: {
-        style: {
-            
-            minHeight: '102px', // override the row height
-            innerWidth:'80px'
-        },
+  rows: {
+    style: {
+      minHeight: '102px',
+      innerWidth: '80px'
     },
-    headCells: {
-        style: {
-            paddingLeft: '18px', // override the cell padding for head cells
-            paddingRight: '18px',
-        },
+  },
+  headCells: {
+    style: {
+      paddingLeft: '18px',
+      paddingRight: '18px',
     },
-    cells: {
-        style: {
-            paddingLeft: '18px', // override the cell padding for data cells
-            paddingRight: '18px',
-        },
+  },
+  cells: {
+    style: {
+      paddingLeft: '18px',
+      paddingRight: '18px',
     },
-    
-    
+  },
 };
 
+//Theme
 createTheme('solarized', {
-    text: {
-      primary: '#268bd2',
-      secondary: '#2aa198',
-    },
-    
-    background: {
-      default: '#002b36',
-    },
-    context: {
-      background: '#cb4b16',
-      text: '#FFFFFF',
-    },
-    divider: {
-      default: '#073642',
-    },
-    action: {
-      button: 'rgba(0,0,0,.54)',
-      hover: 'rgba(0,0,0,.08)',
-      disabled: 'rgba(0,0,0,.12)',
-    },
-  }, 'dark');
-  
+  text: {
+    primary: '#268bd2',
+    secondary: '#2aa198',
+  },
+  background: {
+    default: '#002b36',
+  },
+  context: {
+    background: '#cb4b16',
+    text: '#FFFFFF',
+  },
+  divider: {
+    default: '#073642',
+  },
+  action: {
+    button: 'rgba(0,0,0,.54)',
+    hover: 'rgba(0,0,0,.08)',
+    disabled: 'rgba(0,0,0,.12)',
+  },
+}, 'dark');
 
-
+//Tabla
 const columns = [
-    {
-        name: 'Nombre',
-        selector: row => row.Nombre,
-        sortable: true,
-    },
-    {
-        name: 'JuegosGanados',
-        selector: row => row.JuegosGanados,
-        sortable: true,
-    },
-    {
-        name: 'JuegosPerdidos',
-        selector: row => row.JuegosPerdidos,
-        sortable: true,
-    },
-
+  {
+    name: 'Nombre',
+    selector: row => row.Nombre,
+    sortable: true,
+  },
+  {
+    name: 'Juegos Ganados',
+    selector: row => row.juegosGanados,
+    sortable: true,
+  },
+  {
+    name: 'Juegos Perdidos',
+    selector: row => row.juegosPerdidos,
+    sortable: true,
+  },
 ];
 
-const data = [
+//Tabla(Data)
+const Dashboard = () => {
+  const [data, setData] = useState([
     {
-        id: 1,
-        Nombre:'Leticia',
-        JuegosGanados:'4',
-        JuegosPerdidos:'5',
-
+      id: 1,
+      Nombre: 'Leticia',
+      juegosGanados: 4,
+      juegosPerdidos: 5,
     },
     {
-        id: 2,
-        Nombre:'Rogelio',
-        JuegosGanados:'4',
-        JuegosPerdidos:'5',
+      id: 2,
+      Nombre: 'Rogelio',
+      juegosGanados: 4,
+      juegosPerdidos: 5,
     },
-]
+  ]);
 
+  //Variables juegos ganados y perdidos y nombre
+  const [nombreJugador, setNombreJugador] = useState("");
+  const [juegosGanados, setJuegosGanados] = useState(0);
+  const [juegosPerdidos, setJuegosPerdidos] = useState(0);
+ 
+  //Añadir jugador (Datos)
+  const handleAddRow = () => {
+    setData(prevData => [
+      ...prevData,
+      {
+        id: prevData.length + 1,
+        Nombre: 'Nuevo Jugador',
+        juegosGanados: 0,
+        juegosPerdidos: 0,
+      }
+    ]);
+  };
 
-
-const Dashboard=()=> {
-    return (
-        <div className='rdTable'>
-            <DataTable
-            
-            columns={columns}
-            data={data}
-            customStyles={customStyles}
-            theme="solarized"
-            
-            
-            
-            
-        />
-
-        </div>
-       
-    );
+//Añadir jugador (Boton y tabla)
+  return (
+    <div className='rdTable'>
+      <DataTable
+        columns={columns}
+        data={data}
+        customStyles={customStyles}
+        theme="solarized"
+      />
+      <button onClick={handleAddRow}>Agregar jugador</button>
+    </div>
+  );
 };
 
-export default Dashboard
+export default Dashboard;
+
--- src/Hangman.js
+++ src/Hangman.js
@@ -1,123 +1,193 @@
-import React, { Component } from "react";
-import { randomWord } from './words';
+  import React, { Component } from "react";
+  import { Link } from "react-router-dom"; //ChatGPT 
+  import { randomWord } from './words';
+  
+  import Dashboard from './Dashboard'; // Importa el componente Dashboard
+  //import { useHistory } from 'react-router-dom';
+  import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import "./Hangman.css";
+  import "./Hangman.css";
 
-import img0 from "./images/0.jpg";
-import img1 from "./images/1.jpg";
-import img2 from "./images/2.jpg";
-import img3 from "./images/3.jpg";
-import img4 from "./images/4.jpg";
-import img5 from "./images/5.jpg";
-import img6 from "./images/6.jpg";
+  import img0 from "./images/0.jpg";
+  import img1 from "./images/1.jpg";
+  import img2 from "./images/2.jpg";
+  import img3 from "./images/3.jpg";
+  import img4 from "./images/4.jpg";
+  import img5 from "./images/5.jpg";
+  import img6 from "./images/6.jpg";
 
- export class Hangman extends Component  {
-   
-  /** by default, allow 6 guesses and use provided gallows images. */
-  static defaultProps = {
-    maxWrong: 6,
-    images: [img0, img1, img2, img3, img4, img5, img6]
-  };
-
-  constructor(props) {
-    super(props);
-
-    this.state = { 
-      nWrong: 0, 
-      guessed: new Set(), 
-      // answer: "apple"
-      answer: randomWord() 
+  export class Hangman extends Component  {
+    
+    /** by default, allow 6 guesses and use provided gallows images. */
+    static defaultProps = {
+      maxWrong: 6,
+      images: [img0, img1, img2, img3, img4, img5, img6]
     };
 
-    this.handleGuess = this.handleGuess.bind(this);
-    this.resetGame = this.resetGame.bind(this);
-  }
+    constructor(props) {
+      super(props);
 
-  // reset the game and put things in default
-  resetGame() {
-    this.setState({
-      nWrong: 0,
-      guessed: new Set(),
-      answer: randomWord()
-    });
-  }
+      //Antiguo state
+      // this.state = { 
+      //   nWrong: 0, 
+      //   guessed: new Set(), 
+      //   // answer: "apple"
+      //   answer: randomWord() 
+      // };
+      //Nuevo state
+      this.state = { 
+        nWrong: 0, 
+        guessed: new Set(), 
+        answer: randomWord(),
+        wins: 0, // se agrega una propiedad wins para guardar los juegos ganados
+        losses: 0 // se agrega una propiedad losses para guardar los juegos perdidos
+      };
 
-  /** guessedWord: show current-state of word:
-    if guessed letters are {a,p,e}, show "app_e" for "apple"
-  */
-  guessedWord() {
-    // deconstructor
-    const { answer, guessed } = this.state;
+      this.handleGuess = this.handleGuess.bind(this);
+      this.resetGame = this.resetGame.bind(this);
+      
+      // this.Dashboard = this.Dashboard.bind(this);
+    }
 
-    return answer
-      .split("")
-      .map(ltr => (guessed.has(ltr) ? ltr : "_"));
-  }
+    // //Antiguo reset
+    // // reset the game and put things in default
+    // resetGame() {
+    //   this.setState({
+    //     nWrong: 0,
+    //     guessed: new Set(),
+    //     answer: randomWord()
+    //   });
+    // 
+    // //Nuevo reset 1
+    // resetGame() {
+    //   this.setState({
+    //     nWrong: 0,
+    //     guessed: new Set(),
+    //     answer: randomWord(),
+    //     wins: 0,
+    //     losses: 0
+    //   });
+    // }
+    //Nuevo reset 2
+    resetGame() {
+      console.log(`Wins: ${this.state.wins}, Losses: ${this.state.losses}`);
+      
+      this.setState({
+        nWrong: 0,
+        guessed: new Set(),
+        answer: randomWord()
+      });
+    }
 
-  /** handleGuest: handle a guessed letter:
-    - add to guessed letters
-    - if not in answer, increase number-wrong guesses
-  */
+    /** guessedWord: show current-state of word:
+      if guessed letters are {a,p,e}, show "app_e" for "apple"
+    */
+    guessedWord() {
+      // deconstructor
+      const { answer, guessed } = this.state;
+
+      return answer
+        .split("")
+        .map(ltr => (guessed.has(ltr) ? ltr : "_"));
+    }
+
+    /** handleGuest: handle a guessed letter:
+      - add to guessed letters
+      - if not in answer, increase number-wrong guesses
+    */
+  // //Antiguo handle
+  //     handleGuess(evt) {
+  //     let ltr = evt.target.value;
+
+  //     this.setState(st => ({
+  //       guessed: st.guessed.add(ltr),
+  //       nWrong: st.nWrong + (st.answer.includes(ltr) ? 0 : 1)
+  //     }));
+  //   }
+  //Nuevo handle
   handleGuess(evt) {
     let ltr = evt.target.value;
 
     this.setState(st => ({
       guessed: st.guessed.add(ltr),
-      nWrong: st.nWrong + (st.answer.includes(ltr) ? 0 : 1)
+      nWrong: st.nWrong + (st.answer.includes(ltr) ? 0 : 1),
+      wins: st.wins + (st.answer.split("").every(ltr => st.guessed.has(ltr)) ? 1 : 0),
+      losses: st.losses + (st.nWrong === this.props.maxWrong ? 1 : 0)
     }));
   }
 
-  /** generateButtons: return array of letter buttons to render */
-  generateButtons() {
-    const  { handleGuess } = this;
-    const { guessed } = this.state;
+    /** generateButtons: return array of letter buttons to render */
+    generateButtons() {
+      const  { handleGuess } = this;
+      const { guessed } = this.state;
 
-    return "abcdefghijklmnopqrstuvwxyz".split("").map((ltr, index) => (
-      <button
-        key={index}
-        value={ltr}
-        onClick={handleGuess}
-        disabled={guessed.has(ltr)}
-      >
-        {ltr}
-      </button>
-    ));
-  }
+      return "abcdefghijklmnopqrstuvwxyz".split("").map((ltr, index) => (
+        <button
+          key={index}
+          value={ltr}
+          onClick={handleGuess}
+          disabled={guessed.has(ltr)}
+        >
+          {ltr}
+        </button>
+      ));
+    }
 
-  /** render: render game */
-  render() {
-    const { nWrong, answer} = this.state;
-    const { images, maxWrong } = this.props;
-    
-    let alternateText = `${this.state.nWrong} wrong guesses`;
-    // const { guessedWord, generateButtons } = this;
+    /** render: render game */
+    render() {
+      const { nWrong, answer} = this.state;
+      const { images, maxWrong } = this.props;
 
-    return (
-      <div className='Hangman'>
-         
-        <h1>Hangman</h1>
-        <img src={images[nWrong]} alt={alternateText}/>
-        <p>Intentos malos: {nWrong} De {maxWrong}</p>
+      
+      
+      let alternateText = `${this.state.nWrong} wrong guesses`;
+      // const { guessedWord, generateButtons } = this;
 
+      // function MyComponent() {
+      //   const history = useHistory();
+      // }
+      // function MyComponent() {
+      //   const history = useHistory();
+      
+      //   function handleClick() {
+      //     history.push('/dashboard');
+      //   }
+      // }
+      return (
         
-        { answer === this.guessedWord().join("") ? <p>Ganaste bb!!</p> :
+        <div className='Hangman'>
+          
+          <h1>Hangman</h1>
+          <img src={images[nWrong]} alt={alternateText}/>
+          <p>Intentos malos: {nWrong} De {maxWrong}</p>
 
-          (nWrong === maxWrong ?
-        <div>
-          <p>Perdiste</p>
-          <p>Palabra correcta es: {answer}</p>
+          
+          { answer === this.guessedWord().join("") ? <p>Ganaste bb!!</p> :
+
+            (nWrong === maxWrong ?
+          <div>
+            <p>Perdiste</p>
+            <p>Palabra correcta es: {answer}</p>
+          </div>
+          :
+          <div>
+          <p className='Hangman-word'>{this.guessedWord()}</p>
+          <p className='Hangman-btns'>{this.generateButtons()}</p>
+        </div>)
+        
+        }
+
+        <button id='reset' onClick={this.resetGame}>Reset Game</button>
+        
+        <button id='dash' onClick={() => this.history.push('./Dashboard')}>Dashboard</button>
+        
         </div>
-        :
-        <div>
-        <p className='Hangman-word'>{this.guessedWord()}</p>
-        <p className='Hangman-btns'>{this.generateButtons()}</p>
-      </div>)
-      }
-
-      <button id='reset' onClick={this.resetGame}>Reset Game</button>
-      </div>
-    );
+        
+      );
+      
+    }
+    
   }
-}
+  
 
-export default Hangman;
+  export default Hangman;
