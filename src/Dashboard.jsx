d173b1a9bd3c38345e30f040416bd4c4f1feb1b1
Rogelio Zuñiga <A010127169@my.uvm.edu.mx>
2023-02-24 16:54:27 -0600

    Cambios dashboard

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -4,27 +4,26 @@ import './Dashboard.css';
 
 const customStyles = {
     rows: {
-        style: {
-            
-            minHeight: '102px', // override the row height
-            innerWidth:'80px'
-        },
+      style: {
+        minHeight: '102px', // override the row height
+        innerWidth:'80px'
+      },
     },
     headCells: {
-        style: {
-            paddingLeft: '18px', // override the cell padding for head cells
-            paddingRight: '18px',
-        },
+      style: {
+        paddingLeft: '18px', // override the cell padding for head cells
+        paddingRight: '18px',
+        fontSize: '20px', // increase font size for header cells
+      },
     },
     cells: {
-        style: {
-            paddingLeft: '18px', // override the cell padding for data cells
-            paddingRight: '18px',
-        },
+      style: {
+        paddingLeft: '18px', // override the cell padding for data cells
+        paddingRight: '18px',
+        fontSize: '16px', // increase font size for data cells
+      },
     },
-    
-    
-};
+  };
 
 createTheme('solarized', {
     text: {
@@ -81,23 +80,57 @@ const data = [
     {
         id: 2,
         Nombre:'Rogelio',
-        JuegosGanados:'4',
+        JuegosGanados:'1',
         JuegosPerdidos:'5',
     },
+    {
+        id: 3,
+        Nombre:'Daniel',
+        JuegosGanados:'6',
+        JuegosPerdidos:'20',
+    },
+    {
+        id: 4,
+        Nombre:'Beder',
+        JuegosGanados:'100',
+        JuegosPerdidos:'50',
+    },
+    {
+        id: 5,
+        Nombre:'Alan',
+        JuegosGanados:'40',
+        JuegosPerdidos:'53',
+    },
+    {
+        id: 6,
+        Nombre:'Juan',
+        JuegosGanados:'14',
+        JuegosPerdidos:'25',
+    },
+    {
+        id: 7,
+        Nombre:'Raul',
+        JuegosGanados:'64',
+        JuegosPerdidos:'5',
+    },
+   
 ]
 
 
 
 const Dashboard=()=> {
     return (
+        
+       
+
         <div className='rdTable'>
+        
             <DataTable
             
             columns={columns}
             data={data}
             customStyles={customStyles}
             theme="solarized"
-            
             
             
             
